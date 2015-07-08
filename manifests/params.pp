24aefdd24992793bbe1fd6d13e84a6492db034c2
Vanessa Hamar <hamar@cc.in2p3.fr>
2015-07-08 09:39:24 +0200

    #

--- manifests/params.pp
+++ manifests/params.pp
@@ -30,9 +30,9 @@ class squid3::params {
     }
   }
 
-  $access_log      = [ "${log_directory}/access.log squid" ]
-  $cache_log       = "${log_directory}/cache.log"
-  $cache_store_log = "${log_directory}/store.log"
+  #$access_log      = [ "${log_directory}/access.log squid" ]
+  #$cache_log       = "${log_directory}/cache.log"
+  #$cache_store_log = "${log_directory}/store.log"
 
 }
 
